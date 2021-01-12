e0da0e73a716332f2978aa9fec3fdfd11d0327f4
1dlvb <matushkin-2017@mail.ru>
2021-01-12 21:46:18 +0400

    Errors tracking, add more functionality

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ eel.init('web')
 
 
 @eel.expose
-def tag_select_py(tag_js):
+def tag_select_py(tag_js):                               # get tag from js
     tag = tag_js
-    print("Tag is: {}".format(tag_js))
-    return 'Py says getta fuck out.'
+    print("Tag is: {}".format(tag))
+    return 'Py says whats up.'
+
+
+@eel.expose
+def tag_money_spend_amount_py(ams_js):                   # get Amount of money spent
+    ams = ams_js
+    print("You spent: {}".format(ams))
 
 
 eel.start('index.html', size=(900, 900))
--- web/script.js
+++ web/script.js
@@ -60,10 +60,69 @@ function OpenDebtsWindow()
 
 }
 
-function TagSelect(){
+function expense(){
+	//money variable
+	var ams_js = document.querySelector("#amountOfMoneySpent").value;
+	
+	//tag variables
 	var sel = document.getElementById('inputTagSelect').selectedIndex;
 	var options = document.getElementById('inputTagSelect').options;
 	var tag_js = options[sel].text;
-	eel.tag_select_py(tag_js)
-	console.log(tag_js);
+	if (ams_js < 0.01) {
+		//show error
+		document.getElementById('alert-money').style.display = "block";
+		return false; 
+	}
+	else
+	{
+		//tag
+		eel.tag_select_py(tag_js);
+		console.log(tag_js);
+
+	
+		//money
+		document.getElementById('alert-money').style.display = "none";
+		console.log(ams_js);
+		eel.tag_money_spend_amount_py(ams_js);	
+	}
+}
+
+function amount_of_money_spent(){
+	var ams_js = document.querySelector("#amountOfMoneySpent").value;
+	
+	if (ams_js < 0.01) {
+		document.getElementById('alert-money').style.display = "block";
+		return false; 
+	}
+	else
+	{
+		document.getElementById('alert-money').style.display = "none";
+		console.log(ams_js);
+		eel.tag_money_spend_amount_py(ams_js);	
+	}	
+	
+}
+
+
+
+function tag_select(){
+	
+
+	if (amount_of_money_spent.ams_js < 0.01) {
+		throw new Error("Stopping the function!");
+	}
+	else
+	{
+		var sel = document.getElementById('inputTagSelect').selectedIndex;
+		var options = document.getElementById('inputTagSelect').options;
+		var tag_js = options[sel].text;
+		eel.tag_select_py(tag_js);
+		console.log(tag_js);
+	}
+}
+
+function clear_values_expenses(){
+	document.getElementById('inputTagSelect').value = 'Other'
+	document.getElementById('amountOfMoneySpent').value = ' ';
+
 }
